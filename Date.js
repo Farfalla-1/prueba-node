ba119c4cf6b79e8962cd281c5c156d4d116d2df9
Farfalla-1 <agustinsangiorgi1998@gmail.com>
2020-03-23 16:45:13 -0300

    Agregando 2 archivos

---
+++ Date.js
@@ -0,0 +1,12 @@
+//te muestra la fecha y hora actual
+let fecha = new Date
+
+console.log(fecha)
+
+//Te muestrala la fecha que le pasas a Date
+let fechaDos = new Date(2019, 10, 02)
+
+console.log(fechaDos)
+
+//RECORDAR QUE LOS MESES EMPIEZAN DESDE EL 0, (por ejemplo el mes de Mayo no es el 5, sino que es el 4);
+
---
+++ borrador.js
@@ -0,0 +1,7 @@
+let dias =  ["Lunes", "Martes", "Miercoles"]
+
+let Mayusculas = function(dias){
+    return dias.toUpperCase()
+}
+ let Hola = dias.map(Mayusculas(dias))
+
